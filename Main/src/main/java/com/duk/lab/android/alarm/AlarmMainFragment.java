cd09b2a653306fe54b9017ad25cd0b8f15450294
ndukwon <ndukwon@gmail.com>
2017-02-03 12:00:57 +0900

    develop

--- Main/src/main/java/com/duk/lab/android/alarm/AlarmMainFragment.java
+++ Main/src/main/java/com/duk/lab/android/alarm/AlarmMainFragment.java
@@ -4,14 +4,15 @@ import com.duk.lab.android.R;
 
 import android.app.AlarmManager;
 import android.app.Fragment;
+import android.app.PendingIntent;
 import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
 import android.support.v7.widget.RecyclerView;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
-import android.widget.TextView;
 
 /**
  * Created by dukwonnam on 2017. 1. 24..
@@ -47,17 +48,30 @@ public class AlarmMainFragment extends Fragment implements View.OnClickListener 
 
         switch (v.getId()) {
             case R.id.addAlarm:
-                // TODO: add alarm
+                addAlarm();
                 break;
         }
+    }
+
+    private void addAlarm() {
+        final AlarmManager alarmManager = (AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
+        Intent intent = new Intent(Intent.ACTION_VIEW);
+        PendingIntent pIntent = PendingIntent.getActivity(getActivity(), 0, intent, 0);
+
+        /*
+           RTC : UTC표준시간을 기준으로 하는 명시적인 시간에 intent를 발생
+           RTC_WAKEUP : UTC 표준시간을 기준으로 하는 명시적인 시간에 intent를 발생, 장치를 깨움
+           ELAPSED_REALTIME : 부팅 후 시간을 기준으로 지정된 시간에 intent를 발생
+           ELAPSED_REALTIME_WAKEUP : 부팅 후 시간을 기준으로 지정된 시간에 intent를 발생, 장치를 깨움
+           출처: http://doraeul.tistory.com/73 [도래울]
+         */
+        alarmManager.set(AlarmManager.RTC_WAKEUP, System.currentTimeMillis() + 5000, pIntent);
     }
 
     private void updateAlarmList(View view) {
         if (view == null) {
             return;
         }
-
-        final AlarmManager alarmManager = (AlarmManager)getActivity().getSystemService(Context.ALARM_SERVICE);
 
         mRecyclerView = (RecyclerView)view.findViewById(R.id.currentAlarmList);
         // TODO: setAdapter
