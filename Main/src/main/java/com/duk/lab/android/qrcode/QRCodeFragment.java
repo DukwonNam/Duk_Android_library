7ecc58a635bb7482c1df971765c7203659546f58
ndukwon <ndukwon@gmail.com>
2017-06-02 19:07:47 +0900

    using zxing original qr reader

--- Main/src/main/java/com/duk/lab/android/qrcode/QRCodeFragment.java
+++ Main/src/main/java/com/duk/lab/android/qrcode/QRCodeFragment.java
@@ -5,19 +5,25 @@ import android.content.ActivityNotFoundException;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 
 import com.duk.lab.android.R;
+import com.google.zxing.client.android.Intents;
+import com.google.zxing.integration.android.IntentIntegrator;
+import com.google.zxing.integration.android.IntentResult;
 
 /**
  * Created by dukwonnam on 2017. 1. 6..
  */
 
 public class QRCodeFragment extends Fragment implements View.OnClickListener {
+    private static String TAG = "QRCodeFragment";
 
     private static final int[] ON_CLICK_LISTENER_ID_ARRAY = new int[] {
+            R.id.qrcodeScannerZxing,
             R.id.qrcodeScanner,
             R.id.qrcodeWriter
     };
@@ -44,6 +50,9 @@ public class QRCodeFragment extends Fragment implements View.OnClickListener {
         }
 
         switch (v.getId()) {
+            case R.id.qrcodeScannerZxing:
+                jumpToQRCodeScannerZxing();
+                break;
             case R.id.qrcodeScanner:
                 jumpToQRCodeScanner();
                 break;
@@ -51,6 +60,15 @@ public class QRCodeFragment extends Fragment implements View.OnClickListener {
                 jumpToQRCodeWriter();
                 break;
         }
+    }
+
+    private void jumpToQRCodeScannerZxing() {
+        IntentIntegrator integrator = new IntentIntegrator(getActivity());
+        integrator.setDesiredBarcodeFormats(IntentIntegrator.QR_CODE_TYPES);
+        Intent intent = integrator.createScanIntent();
+        intent.putExtra(Intents.Scan.CAMERA_ID, 1); // Front camera
+
+        startActivityForResult(intent, IntentIntegrator.REQUEST_CODE);
     }
 
     private void jumpToQRCodeScanner() {
@@ -72,6 +90,21 @@ public class QRCodeFragment extends Fragment implements View.OnClickListener {
         } catch (ActivityNotFoundException e) {
             e.printStackTrace();
             // Do nothing
+        }
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        Log.i(TAG, "onActivityResult requestCode=" + requestCode + ", resultCode=" + resultCode + ", data=" + data);
+
+        IntentResult qrResult = IntentIntegrator.parseActivityResult(requestCode, resultCode, data);
+        if (qrResult != null) {
+            Log.i(TAG, "onActivityResult qrResult.getContents()=" + qrResult.getContents());
+            Log.i(TAG, "onActivityResult qrResult.getErrorCorrectionLevel()=" + qrResult.getErrorCorrectionLevel());
+            Log.i(TAG, "onActivityResult qrResult.getFormatName()=" + qrResult.getFormatName());
+            Log.i(TAG, "onActivityResult qrResult.getOrientation()=" + qrResult.getOrientation());
+            Log.i(TAG, "onActivityResult qrResult.getRawBytes()=" + qrResult.getRawBytes());
         }
     }
 }
