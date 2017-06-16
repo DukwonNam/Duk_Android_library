b1ec311feb7bdd856e1aedbf4ce3b34700dc87ed
ndukwon <ndukwon@gmail.com>
2017-06-16 19:03:12 +0900

    added bluetooth scanning

---
+++ Main/src/main/java/com/duk/lab/android/bluetooth/BTDeviceListAdapter.java
@@ -0,0 +1,135 @@
+package com.duk.lab.android.bluetooth;
+
+import android.bluetooth.BluetoothDevice;
+import android.content.Context;
+import android.support.v7.widget.RecyclerView;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import com.duk.lab.android.R;
+
+import java.util.ArrayList;
+import java.util.List;
+
+/**
+ * Created by dukwonnam on 2017. 6. 16..
+ */
+
+public class BTDeviceListAdapter extends RecyclerView.Adapter {
+
+    interface BTDeviceListItemListener {
+        void onClickItem(BluetoothDevice device);
+    }
+
+    class BTItem {
+        public static final int VIEWTYPE_DEVICE = 0;
+        public static final int VIEWTYPE_LINE = 1;
+
+        final BluetoothDevice device;
+        final int viewType;
+
+        BTItem(BluetoothDevice device, int viewType) {
+            this.device = device;
+            this.viewType = viewType;
+        }
+    }
+
+    private final List<BTItem> mBondedDeviceList = new ArrayList<>();
+    private final List<BTItem> mNewDeviceList = new ArrayList<>();
+    private final Context mContext;
+    private final BTDeviceListItemListener mListener;
+
+    BTDeviceListAdapter(Context context, BTDeviceListItemListener listener) {
+        mContext = context;
+        mListener = listener;
+    }
+
+    public void add(BluetoothDevice device) {
+        switch (device.getBondState()) {
+            case BluetoothDevice.BOND_BONDED:
+                mBondedDeviceList.add(new BTItem(device, BTItem.VIEWTYPE_DEVICE));
+                break;
+            case BluetoothDevice.BOND_BONDING:
+            case BluetoothDevice.BOND_NONE:
+            default:
+                mNewDeviceList.add(new BTItem(device, BTItem.VIEWTYPE_DEVICE));
+                break;
+        }
+    }
+
+    public void clear() {
+        mNewDeviceList.clear();
+        mBondedDeviceList.clear();
+        mBondedDeviceList.add(new BTItem(null, BTItem.VIEWTYPE_LINE));
+    }
+
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(mContext).inflate(R.layout.bt_device_item, parent, false);
+        return new BTDeviceItemHolder(view);
+    }
+
+    @Override
+    public void onBindViewHolder(RecyclerView.ViewHolder holder, int position) {
+        final BluetoothDevice device;
+        if (mNewDeviceList.size() < position) {
+            device = mNewDeviceList.get(position).device;
+        } else {
+            device = mBondedDeviceList.get(position - mNewDeviceList.size()).device;
+        }
+
+        BTDeviceItemHolder btDeviceItemHolder = (BTDeviceItemHolder)holder;
+        switch (device.getBondState()) {
+            case BluetoothDevice.BOND_BONDED:
+                btDeviceItemHolder.bondState.setText("BONDED");
+                break;
+            case BluetoothDevice.BOND_BONDING:
+                btDeviceItemHolder.bondState.setText("BONDING");
+                break;
+            case BluetoothDevice.BOND_NONE:
+            default:
+                btDeviceItemHolder.bondState.setText("BONDING");
+                break;
+        }
+        btDeviceItemHolder.deviceName.setText(device.getName());
+        btDeviceItemHolder.deviceAddress.setText(device.getAddress());
+    }
+
+    @Override
+    public int getItemCount() {
+        return mNewDeviceList.size() + mBondedDeviceList.size();
+    }
+
+    class BTDeviceItemHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView bondState;
+        TextView deviceName;
+        TextView deviceAddress;
+
+        public BTDeviceItemHolder(View itemView) {
+            super(itemView);
+
+            itemView.setOnClickListener(this);
+
+            bondState = (TextView)itemView.findViewById(R.id.bondState);
+            deviceName = (TextView)itemView.findViewById(R.id.deviceName);
+            deviceAddress = (TextView)itemView.findViewById(R.id.deviceAddress);
+        }
+
+        @Override
+        public void onClick(View v) {
+            int position = getAdapterPosition();
+            final BluetoothDevice device;
+            if (mNewDeviceList.size() < position) {
+                device = mNewDeviceList.get(position).device;
+            } else {
+                device = mBondedDeviceList.get(position - mNewDeviceList.size()).device;
+            }
+
+            if (mListener != null) {
+                mListener.onClickItem(device);
+            }
+        }
+    }
+}
--- Main/src/main/java/com/duk/lab/android/bluetooth/BluetoothMainFragment.java
+++ Main/src/main/java/com/duk/lab/android/bluetooth/BluetoothMainFragment.java
@@ -1,11 +1,23 @@
 package com.duk.lab.android.bluetooth;
 
 import android.app.Fragment;
+import android.bluetooth.BluetoothAdapter;
+import android.bluetooth.BluetoothDevice;
+import android.bluetooth.BluetoothManager;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.os.Build;
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.v7.widget.LinearLayoutManager;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.Button;
 
 import com.duk.lab.android.R;
 
@@ -13,9 +25,17 @@ import com.duk.lab.android.R;
  * Created by Duk on 2016-12-19.
  */
 
-public class BluetoothMainFragment extends Fragment implements View.OnClickListener {
-    private static final int[] CLICK_EVENT_ID_ARRAY = new int[] {
-    };
+public class BluetoothMainFragment extends Fragment implements View.OnClickListener{
+    private static final String TAG = "BluetoothMainFragment";
+    private static final String BT_SCAN_START = "Scan BT";
+    private static final String BT_SCAN_STOP = "Stop BT";
+
+    private static final int REQUEST_BT = 99;
+
+    private RecyclerView mBTFoundListView;
+    private BTDeviceListAdapter mBTDeviceListAdapter;
+    private Button mToggleScanBt;
+    private BluetoothAdapter mBTAdapter;
 
     @Nullable
     @Override
@@ -23,11 +43,23 @@ public class BluetoothMainFragment extends Fragment implements View.OnClickListe
         super.onCreateView(inflater, container, savedInstanceState);
 
         View view = inflater.inflate(R.layout.bluetooth_main, container, false);
-        for (int resId : CLICK_EVENT_ID_ARRAY) {
-            view.findViewById(resId).setOnClickListener(this);
-        }
+
+        mBTFoundListView = (RecyclerView)view.findViewById(R.id.btFoundList);
+        mBTFoundListView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.VERTICAL, false));
+        mBTFoundListView.setHasFixedSize(true);
+        mBTDeviceListAdapter = new BTDeviceListAdapter(getActivity(), mBTDeviceItemListener);
+        mBTFoundListView.setAdapter(mBTDeviceListAdapter);
+
+        mToggleScanBt = (Button)view.findViewById(R.id.toggleScanBt);
+        mToggleScanBt.setOnClickListener(this);
 
         return view;
+    }
+
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        stopScan();
     }
 
     @Override
@@ -37,7 +69,111 @@ public class BluetoothMainFragment extends Fragment implements View.OnClickListe
         }
 
         switch (v.getId()) {
+            case R.id.toggleScanBt:
+                if (BT_SCAN_START.equalsIgnoreCase(mToggleScanBt.getText().toString())) {
+                    startScan();
+                } else {
+                    stopScan();
+                }
+                break;
         }
     }
 
+    private void startScan() {
+        mToggleScanBt.setText(BT_SCAN_STOP);
+
+        if (Build.VERSION.SDK_INT <= 17) {
+            mBTAdapter = BluetoothAdapter.getDefaultAdapter();
+        } else {
+            BluetoothManager btMgr = (BluetoothManager)getActivity().getSystemService(Context.BLUETOOTH_SERVICE);
+            mBTAdapter = btMgr.getAdapter();
+        }
+        Log.d(TAG, "startScan mBTAdapter=" + mBTAdapter);
+
+        if (mBTAdapter == null) {
+            return;
+        }
+
+        if (!mBTAdapter.isEnabled()) {
+            Log.d(TAG, "startScan mBTAdapter.isEnabled() is false");
+            Intent enableBT = new Intent(BluetoothAdapter.ACTION_REQUEST_ENABLE);
+            startActivityForResult(enableBT, REQUEST_BT);
+            return;
+        }
+
+        if (mBTAdapter.isDiscovering()) {
+            Log.d(TAG, "startScan mBTAdapter.isDiscovering");
+            mBTAdapter.cancelDiscovery();
+        }
+
+        mBTDeviceListAdapter.clear();
+        IntentFilter filter = new IntentFilter();
+        filter.addAction(BluetoothDevice.ACTION_FOUND);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_STARTED);
+        filter.addAction(BluetoothAdapter.ACTION_DISCOVERY_FINISHED);
+        getActivity().registerReceiver(mBTFoundReceiver, filter);
+
+        boolean result = mBTAdapter.startDiscovery();
+        Log.d(TAG, "startScan mBTAdapter.startDiscovery result=" + result);
+    }
+
+    private void stopScan() {
+        mToggleScanBt.setText(BT_SCAN_START);
+
+        Log.d(TAG, "stopScan mBTAdapter=" + mBTAdapter);
+        if (mBTAdapter == null) {
+            return;
+        }
+
+        getActivity().unregisterReceiver(mBTFoundReceiver);
+        mBTAdapter.cancelDiscovery();
+        mBTAdapter = null;
+    }
+
+    private BroadcastReceiver mBTFoundReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "mBTFoundReceiver onReceive intent=" + intent);
+
+            if (BluetoothDevice.ACTION_FOUND.equals(action)) {
+                BluetoothDevice device = intent.getParcelableExtra(BluetoothDevice.EXTRA_DEVICE);
+                Log.d(TAG, "device=" + device);
+
+                mBTDeviceListAdapter.add(device);
+                mBTDeviceListAdapter.notifyDataSetChanged();
+            } else if (BluetoothAdapter.ACTION_DISCOVERY_FINISHED.equals(action)) {
+                stopScan();
+            }
+        }
+    };
+
+    BTDeviceListAdapter.BTDeviceListItemListener mBTDeviceItemListener = new BTDeviceListAdapter.BTDeviceListItemListener() {
+        @Override
+        public void onClickItem(BluetoothDevice device) {
+            stopScan();
+            if (device.getBondState() == BluetoothDevice.BOND_BONDED) {
+                return;
+            }
+
+            IntentFilter filter = new IntentFilter();
+            filter.addAction(BluetoothDevice.ACTION_PAIRING_REQUEST);
+            getActivity().registerReceiver(mBTConectReceiver, filter);
+
+            if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.KITKAT) {
+                device.createBond();
+            }
+        }
+    };
+
+    private BroadcastReceiver mBTConectReceiver = new BroadcastReceiver() {
+        @Override
+        public void onReceive(Context context, Intent intent) {
+            String action = intent.getAction();
+            Log.d(TAG, "mBTConectReceiver onReceive intent=" + intent);
+
+            if (BluetoothDevice.ACTION_PAIRING_REQUEST.equals(action)) {;
+            }
+        }
+    };
 }
